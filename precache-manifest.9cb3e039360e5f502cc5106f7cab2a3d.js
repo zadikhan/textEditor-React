062f0a5be93e07297119bbb5ffdf967f195a07ac
Mohd Azam Khan <zadkhan51@gmail.com>
2022-09-05 01:08:13 +0530

    Updates

---
+++ precache-manifest.9cb3e039360e5f502cc5106f7cab2a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "acc828cf02cb95b5af3d",
+    "url": "/textEditor-React/static/css/main.20e3e417.chunk.css"
+  },
+  {
+    "revision": "acc828cf02cb95b5af3d",
+    "url": "/textEditor-React/static/js/main.acc828cf.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textEditor-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textEditor-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "3ae0e3c43aa9db7191f6",
+    "url": "/textEditor-React/static/js/runtime~main.3ae0e3c4.js"
+  },
+  {
+    "revision": "b3f7b3fdf835fb5505ad6e79ff294d96",
+    "url": "/textEditor-React/index.html"
+  }
+];
